b7e40e6e04027113a8e736d52aaf85951fd2de98
Abid Irsyad <abidichadd@gmail.com>
2026-02-06 18:23:47 +0700

    week01: (tugas 1) convert checkSize to expression body function

--- src/oop_111284_Abid_Irsyad_Dinejad/week01/CircleCalculator.kt
+++ src/oop_111284_Abid_Irsyad_Dinejad/week01/CircleCalculator.kt
@@ -1,23 +1,13 @@
-package oop_111284_Abid_Irsyad_Dinejad.week01
+package oop_001_johnthor.week01
 
 fun main(args: Array<String>) {
-    // Menggunakan val dan Type Inference (tanpa : Double)
     val radius = 7.0
     val pi = 3.14
-
-    // Calculation
     val area = pi * radius * radius
 
-    // String Template (menggunakan $variable)
     println("Radius: $radius, Area: $area")
 
-    checkSize(area)
+    println(checkSize(area))
 }
 
-fun checkSize(area: Double) {
-    if (area > 100) {
-        println("This is a Big Circle")
-    } else {
-        println("This is a Small Circle")
-    }
-}
+fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
