9976562cc14f780e5200b47e51e7d7378be55134
Abid Irsyad <abidichadd@gmail.com>
2026-02-06 18:22:30 +0700

    week01: (tugas 1) refactor variables to val and use string templates

--- src/oop_111284_Abid_Irsyad_Dinejad/week01/CircleCalculator.kt
+++ src/oop_111284_Abid_Irsyad_Dinejad/week01/CircleCalculator.kt
@@ -1,13 +1,23 @@
 package oop_111284_Abid_Irsyad_Dinejad.week01
 
 fun main(args: Array<String>) {
+    // Menggunakan val dan Type Inference (tanpa : Double)
     val radius = 7.0
     val pi = 3.14
+
+    // Calculation
     val area = pi * radius * radius
 
+    // String Template (menggunakan $variable)
     println("Radius: $radius, Area: $area")
 
-    println(checkSize(area))
+    checkSize(area)
 }
 
-fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
+fun checkSize(area: Double) {
+    if (area > 100) {
+        println("This is a Big Circle")
+    } else {
+        println("This is a Small Circle")
+    }
+}
