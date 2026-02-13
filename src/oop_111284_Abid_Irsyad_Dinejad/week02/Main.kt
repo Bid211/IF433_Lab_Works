656e5e1ff2c7f3304ecc79b65969183b8f73236c
Abid Irsyad <abidichadd@gmail.com>
2026-02-13 10:38:33 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_111284_Abid_Irsyad_Dinejad/week02/Main.kt
@@ -0,0 +1,29 @@
+package oop_111284_Abid_Irsyad_Dinejad.week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Nama:")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if (nim.length !=5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM Harus 5 Karakter")
+
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai. ")
+
+    }
+}
