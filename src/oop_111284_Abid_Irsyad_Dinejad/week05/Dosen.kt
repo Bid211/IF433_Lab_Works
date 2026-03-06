6d604664c517045c53ec78f1b727e284a519ed53
Abid Irsyad <abidichadd@gmail.com>
2026-03-06 10:22:10 +0700

    week05: create Dosen class inheriting Pegawai

---
+++ src/oop_111284_Abid_Irsyad_Dinejad/week05/Dosen.kt
@@ -0,0 +1,12 @@
+package oop_111284_Abid_Irsyad_Dinejad.week05
+
+class Dosen(nama:String, val nidn: Pegawai) : Pegawai(nama){
+    override fun bekerja() {
+        println("[$nama] Sedang menyiapkan materi perkuliahan dan merevisi RKPS.")
+    }
+
+    fun mengajar() {
+        println("[$nama] Sedang mengajar di kelas.")
+    }
+}
+
