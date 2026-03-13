ce240cce7d5e06e23d65cbebbf95dccdf55ae21d
Abid Irsyad <abidichadd@gmail.com>
2026-03-13 10:47:12 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_111284_Abid_Irsyad_Dinejad/week06/Mandiri/SmartCCTV.kt
+++ src/oop_111284_Abid_Irsyad_Dinejad/week06/Mandiri/SmartCCTV.kt
@@ -1,2 +1,17 @@
 package oop_111284_Abid_Irsyad_Dinejad.week06.Mandiri
 
+class SmartCCTV(override val id: String, override val name: String) : SmartDevice, Switchable, Recordable {
+
+    override fun turnOn() {
+        println("CCTV [$name] dinyalakan.")
+        startRecord()
+    }
+
+    override fun turnOff() {
+        println("CCTV [$name] dimatikan.")
+    }
+
+    override fun startRecord() {
+        println("CCTV [$name] mulai merekam video keamanan...")
+    }
+}
--- src/oop_111284_Abid_Irsyad_Dinejad/week06/Mandiri/SmartHomeHub.kt
+++ src/oop_111284_Abid_Irsyad_Dinejad/week06/Mandiri/SmartHomeHub.kt
@@ -1,2 +1,32 @@
 package oop_111284_Abid_Irsyad_Dinejad.week06.Mandiri
 
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("Perangkat [${device.name}] ditambahkan ke Hub.")
+    }
+
+    fun turnOffAllSwitches() {
+        println("\n--- Mematikan Semua Perangkat ---")
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode() {
+        println("\n--- Mengaktifkan Mode Keamanan ---")
+        for (device in devices) {
+            if (device is Recordable) {
+                device.startRecord()
+            }
+
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
+}
