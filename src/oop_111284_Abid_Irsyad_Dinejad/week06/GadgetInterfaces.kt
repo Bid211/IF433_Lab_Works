9b00967bb22f17bc307ee5ac3046a35f57897dd6
Abid Irsyad <abidichadd@gmail.com>
2026-03-13 10:17:40 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_111284_Abid_Irsyad_Dinejad/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_111284_Abid_Irsyad_Dinejad.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif")}
+}
+
+interface Phone {
+    fun turnOn() {println("Sinyal seluler mencari jaringan")}
+}
