d329b0306d097cd1d061b3d85bb84f4923bcf7e6
Abid Irsyad <abidichadd@gmail.com>
2026-03-27 10:35:48 +0700

    week07: create ApiResponse sealed class

--- src/oop_111284_Abid_Irsyad_Dinejad/week07/SystemStates.kt
+++ src/oop_111284_Abid_Irsyad_Dinejad/week07/SystemStates.kt
@@ -3,3 +3,12 @@ package oop_111284_Abid_Irsyad_Dinejad.week07
 enum class Appstate {
     STARTING, RUNNING, STOPPED
 }
+
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Failure(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
+    }
+
+
+
