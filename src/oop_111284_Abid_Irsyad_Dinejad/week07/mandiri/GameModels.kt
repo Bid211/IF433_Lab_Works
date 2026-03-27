ec93f1707208e4f117aab16a7963aa140fe88c99
Abid Irsyad <abidichadd@gmail.com>
2026-03-27 10:48:22 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_111284_Abid_Irsyad_Dinejad/week07/mandiri/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_111284_Abid_Irsyad_Dinejad.week07.mandiri
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
