d016c64239fb4d515adbabac2dc6688a1e694f60
Abid Irsyad <abidichadd@gmail.com>
2026-04-17 11:03:53 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_111284_Abid_Irsyad_Dinejad/week08/Mandiri/EcommerceModels.kt
@@ -0,0 +1,16 @@
+package oop_111284_Abid_Irsyad_Dinejad.week08.Mandiri
+
+selaed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
+
