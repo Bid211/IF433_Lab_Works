414e1e36dd68fed5291de5872f4dec95142688bc
Abid Irsyad <abidichadd@gmail.com>
2026-04-17 11:06:56 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_111284_Abid_Irsyad_Dinejad/week08/Mandiri/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_111284_Abid_Irsyad_Dinejad.week08.Mandiri;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
