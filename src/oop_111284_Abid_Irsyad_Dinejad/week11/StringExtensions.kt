6d341f0d08060064cedf55c1fccfbc8ff3500c51
Abid Irsyad <abidichadd@gmail.com>
2026-05-08 09:53:00 +0700

    week11: create string extension functions

---
+++ src/oop_111284_Abid_Irsyad_Dinejad/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_111284_Abid_Irsyad_Dinejad.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
