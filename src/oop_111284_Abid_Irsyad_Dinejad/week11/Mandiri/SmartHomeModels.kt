855ae2b8ea91039d55c87a97e2935bc637d1ac82
Abid Irsyad <abidichadd@gmail.com>
2026-05-08 10:34:58 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_111284_Abid_Irsyad_Dinejad/week11/Mandiri/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_111284_Abid_Irsyad_Dinejad.week11.Mandiri
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
