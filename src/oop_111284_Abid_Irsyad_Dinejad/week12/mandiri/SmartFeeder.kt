d5598d272c141ece36c54e101fe24b36b3ea04b1
Abid Irsyad <abidichadd@gmail.com>
2026-05-15 10:18:40 +0700

    week12:(task)implement hardware validation with throw

--- src/oop_111284_Abid_Irsyad_Dinejad/week12/mandiri/SmartFeeder.kt
+++ src/oop_111284_Abid_Irsyad_Dinejad/week12/mandiri/SmartFeeder.kt
@@ -3,4 +3,8 @@ package oop_111284_Abid_Irsyad_Dinejad.week12.mandiri
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0gr" }
     return 0
+
+    if (isJammed) {
+        throw DispenserJamException()
+    }
 }
