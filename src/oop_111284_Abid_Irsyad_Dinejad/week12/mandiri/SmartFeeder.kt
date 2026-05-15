65bc3b0690fab70ef702f0a30d7d6c9eccca06b6
Abid Irsyad <abidichadd@gmail.com>
2026-05-15 10:22:55 +0700

    week12:(task)execute feeding using runCatching

--- src/oop_111284_Abid_Irsyad_Dinejad/week12/mandiri/SmartFeeder.kt
+++ src/oop_111284_Abid_Irsyad_Dinejad/week12/mandiri/SmartFeeder.kt
@@ -30,3 +30,7 @@ fun main() {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
 
+    println("\n=== JADWAL MAKAN SORE ===")
+    runCatching {
+        dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
+    }
