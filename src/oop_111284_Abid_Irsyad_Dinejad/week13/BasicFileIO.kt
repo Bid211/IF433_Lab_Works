91b1ec42b27513934ca53b75a23e6510f3f345bf
Abid Irsyad <abidichadd@gmail.com>
2026-05-22 10:30:08 +0700

    week13: implement basic writeText function

---
+++ src/oop_111284_Abid_Irsyad_Dinejad/week13/BasicFileIO.kt
@@ -0,0 +1,11 @@
+package oop_111284_Abid_Irsyad_Dinejad.week13
+
+import java.io.File
+
+fun main() {
+    println("=== Test Write Text ===")
+    val file: File = File("notes.txt")
+    file.writeText("Line 1: Sistem Inisialisasi. \n")
+    println("File berhasil dibuat dan ditulis")
+
+}
