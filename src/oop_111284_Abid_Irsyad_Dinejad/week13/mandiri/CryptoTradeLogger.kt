e9a416d2e975beb0ab0ea3aa42f4336bb4c27555
Abid Irsyad <abidichadd@gmail.com>
2026-05-22 11:36:37 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_111284_Abid_Irsyad_Dinejad/week13/mandiri/CryptoTradeLogger.kt
@@ -0,0 +1,12 @@
+package oop_111284_Abid_Irsyad_Dinejad.week13.mandiri
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
