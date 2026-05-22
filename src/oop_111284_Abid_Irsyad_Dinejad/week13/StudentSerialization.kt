10ff78dc8eb9695f3f79b45c27a101b33faef4e7
Abid Irsyad <abidichadd@gmail.com>
2026-05-22 10:53:13 +0700

    week13: create Student model

---
+++ src/oop_111284_Abid_Irsyad_Dinejad/week13/StudentSerialization.kt
@@ -0,0 +1,8 @@
+package oop_111284_Abid_Irsyad_Dinejad.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(var name: String, val age: Int, val gpa: Double) {
+
+}
