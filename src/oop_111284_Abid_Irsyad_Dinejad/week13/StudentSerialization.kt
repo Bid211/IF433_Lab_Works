5e5a3425e20da23cf6a3cfdcf49082f297eaa0d3
Abid Irsyad <abidichadd@gmail.com>
2026-05-22 11:02:40 +0700

    week13: implement toCsv and fromCsv logic

--- src/oop_111284_Abid_Irsyad_Dinejad/week13/StudentSerialization.kt
+++ src/oop_111284_Abid_Irsyad_Dinejad/week13/StudentSerialization.kt
@@ -3,6 +3,11 @@ package oop_111284_Abid_Irsyad_Dinejad.week13
 import java.io.File
 import java.io.FileNotFoundException
 
-data class Student(var name: String, val age: Int, val gpa: Double) {
+data class Student(var name: String, val age: Int, val gpa: Double)
 
+fun Student.tocsv(): String = "$name, $age, $gpa"
+
+fun fromCsv(line: String): Student {
+    val parts = line.split(",")
+    return Student(parts[0], parts[1].toInt(), parts[2].toDouble())
 }
