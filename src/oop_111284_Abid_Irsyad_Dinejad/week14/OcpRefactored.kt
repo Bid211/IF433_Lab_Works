32429ec2a40406e9a82ea86ca6797c0bd1271a54
Abid Irsyad <abidichadd@gmail.com>
2026-05-29 08:39:09 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ src/oop_111284_Abid_Irsyad_Dinejad/week14/OcpRefactored.kt
@@ -0,0 +1,13 @@
+package oop_111284_Abid_Irsyad_Dinejad.week14
+
+interface DiscountStrategy {
+    fun apply(price: Double): Double
+}
+
+class StudentDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.80
+}
+
+class MemoDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.85
+}
